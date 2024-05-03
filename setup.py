42362e1e2b6025dd1d94a5d17997fd8598d6e6ba
Atharva <greenmyworld1@gmail.com>
2024-05-03 16:46:21 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI-Doc',
+    version= '0.0.0',
+    author= 'Atharva',
+    author_email= 'greenmyworld1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
